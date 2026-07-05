c8c2a3a728748e775c55456558149e655325472f
davioficiala <daviwalker813@gmail.com>
2026-07-05 18:21:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,65 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/fireba
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAE2VcJyqu01rtqlgVoMg634FFfTGxiRgc",
-  authDomain: "brain-orcamento.firebaseapp.com",
-  projectId: "brain-orcamento",
-  storageBucket: "brain-orcamento.firebasestorage.app",
-  messagingSenderId: "991853359315",
-  appId: "1:991853359315:web:b2270aab447a853b212426"
+apiKey: "AIzaSyAE2VcJyqu01rtqlgVoMg634FFfTGxiRgc",
+authDomain: "brain-orcamento.firebaseapp.com",
+projectId: "brain-orcamento",
+storageBucket: "brain-orcamento.firebasestorage.app",
+messagingSenderId: "991853359315",
+appId: "1:991853359315:web:b2270aab447a853b212426"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-async function teste(){
+let produtos=[];
+
+async function carregar(){
 
 const snap = await getDocs(collection(db,"dados"));
 
 snap.forEach((doc)=>{
 
-console.log("Documento:", doc.id);
+produtos.push({
+titulo: doc.id,
+...doc.data()
+});
 
-console.log("Dados:", doc.data());
+});
+
+mostrar(produtos);
+
+}
+
+function mostrar(lista){
+
+const vitrine=document.getElementById("vitrine");
+
+vitrine.innerHTML="";
+
+lista.forEach((p)=>{
+
+vitrine.innerHTML += `
+<div>
+${p.titulo}
+</div>
+`;
 
 });
 
 }
 
-teste();
+document.getElementById("buscador")
+.addEventListener("input",(e)=>{
+
+const valor=e.target.value.toLowerCase();
+
+const resultado=produtos.filter(p=>
+p.titulo.toLowerCase().includes(valor)
+);
+
+mostrar(resultado);
+
+});
+
+carregar();
